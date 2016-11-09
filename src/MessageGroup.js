421d0712dd3104d1b639fa1070993c39b062f5e1
mbilker <me@mbilker.us>
2016-11-09 08:08:05 +0000

    MessageGroup: linkify the content

--- src/MessageGroup.js
+++ src/MessageGroup.js
@@ -1,11 +1,27 @@
 "use strict";
 
 const h = require('hyperscript');
+const linkify = require('linkifyjs/string');
 
 const Embed = require('./Embed');
 
+function textToLinks(content) {
+  return linkify(content, {
+    attributes: {
+      rel: 'noreferrer'
+    },
+    validate: {
+      url: function(value) {
+        return /^(http)s?:\/\//.test(value);
+      }
+    }
+  });
+}
+
 function MessageGroup({ message }) {
   const embeds = message.embeds.map((embed) => Embed({ embed }));
+  const contentNode = h('div.markup', message.content);
+  contentNode.innerHTML = textToLinks(contentNode.innerHTML);
 
   return h('div.message-group',
     h(`div.avatar-large.animate.avatar_${message.author.id}`),
@@ -18,9 +34,7 @@ function MessageGroup({ message }) {
           h('span.highlight-separator', ' - '),
           h('span.timestamp', message.timestamp)
         ),
-        h('div.message-text',
-          h('div.markup', message.content)
-        ),
+        h('div.message-text', contentNode),
         h('div.accessory', embeds)
       )
     )
